0109fbf15591a20a87406d7f1e83cf7b6faefdee
Zyresss <angaryacine65@gmail.com>
2025-12-06 08:29:47 +0100

    Multithread implementation with file test

--- server.c
+++ server.c
@@ -5,10 +5,79 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
-int main() {
+void *handle_client(void *client_socket_ptr)
+{
+    int client_fd = *(int *)client_socket_ptr;
+
+    char buffer[30000] = {0};
+    long data = read(client_fd, buffer, 30000);
+    char method[10] = {0};
+    char url[100] = {0};
+    sscanf(buffer, "%s %s", method, url); // sscanf
+    printf("Method: %s, URL: %s\n", method, url);
+
+    char *file_name;
+    if(strcmp(url, "/") == 0) {
+        file_name = "index.html";
+
+    } else {
+        file_name = url + 1; // + for skiping the '/'
+    }
+    printf("%s\n", file_name);
+
+    // printf("-------client request--------\n");
+    // printf("the data is : %s\n", buffer);z
+    // printf("-----------------------------\n");
+
+    FILE *fp;
+    fp = fopen(file_name, "r");
+    if (fp == NULL)
+    {
+        printf("File not found: %s\n", file_name);
+        char *not_found_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>404 Not Found</h1>";
+        write(client_fd, not_found_response, strlen(not_found_response));
+        close(client_fd);
+        free(client_socket_ptr);
+        return NULL;
+    }
+    char response_data[30000] = {0};
+    fread(response_data, 30000, 1, fp);
+    fclose(fp);
+
+    char* content_type;
+    if(strstr(file_name, ".css")) {
+        content_type = "text/css";
+
+    } else if(strstr(file_name, ".html")) {
+        content_type = "text/html";
+    } else if(strstr(file_name, ".js")){
+        content_type = "application/javascript";
+    } else if(strstr(file_name, ".jpg") || strstr(file_name, ".jpeg")) {
+        content_type = "image/jpeg";
+    } else if(strstr(file_name, ".png")) {
+        content_type = "image/png";
+    } else {
+        content_type = "text/plain";
+    }
+
+    char header[2048] = {0};
+    sprintf(header, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\n\r\n", content_type); // sprintf it formats and stores the string in header from the library <string.h>
+    write(client_fd, header, strlen(header));
+    write(client_fd, response_data, strlen(response_data));
+    printf("Response sent to browser!\n");
+
+    free(client_socket_ptr);
+    close(client_fd);
+    return NULL;
+}
+
+int main()
+{
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_fd <= 0) {
+    if (server_fd <= 0)
+    {
         perror("Socket creation failed");
         exit(1);
     }
@@ -21,13 +90,15 @@ int main() {
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     int bind_stat = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    if(bind_stat < 0) {
+    if (bind_stat < 0)
+    {
         perror("bind failed");
         exit(1);
     }
 
     int listen_stat = listen(server_fd, 10);
-    if(listen_stat < 0) {
+    if (listen_stat < 0)
+    {
         perror("listen failed");
         exit(1);
     }
@@ -38,38 +109,50 @@ int main() {
     while (1)
     {
         int client_fd = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-        if(client_fd < 0){
+        if (client_fd < 0)
+        {
             perror("accept failed");
-            exit(1);    
+            exit(1);
         }
-        printf("connection accepted");
-    
-        char buffer[30000] = {0};
-        long data = read(client_fd, buffer, 30000); 
-    
-        // printf("-------client request--------\n");
-        // printf("the data is : %s\n", buffer);
-        // printf("-----------------------------\n");
+        printf("connection accepted\n");
 
-        FILE* fp;
-        fp = fopen("index.html", "r");
-        if (fp == NULL) {
-            perror("Failed to open file");
-            close(client_fd);
-            continue;
+        int *pclient = malloc(sizeof(int));
+        *pclient = client_fd;
+        pthread_t thread_id;
+        int thread_stat = pthread_create(&thread_id, NULL, handle_client, pclient);
+        if(thread_stat != 0) {
+            perror("failed to create thread!");
+            exit(1);
         }
-        char response_data[30000] = {0};
-        fread(response_data, 30000, 1, fp);
-        fclose(fp);
+        pthread_detach(thread_id); //
 
-    
-        char header[2048] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-        write(client_fd, header, strlen(header));
-        write(client_fd, response_data, strlen(response_data)); 
-        printf("Response sent to browser!\n");
-        close(client_fd);
+
+
+        // char buffer[30000] = {0};
+        // long data = read(client_fd, buffer, 30000);
+
+        // // printf("-------client request--------\n");
+        // // printf("the data is : %s\n", buffer);
+        // // printf("-----------------------------\n");
+
+        // FILE* fp;
+        // fp = fopen("index.html", "r");
+        // if (fp == NULL) {
+        //     perror("Failed to open file");
+        //     close(client_fd);
+        //     continue;
+        // }
+        // char response_data[30000] = {0};
+        // fread(response_data, 30000, 1, fp);
+        // fclose(fp);
+
+        // char header[2048] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+        // write(client_fd, header, strlen(header));
+        // write(client_fd, response_data, strlen(response_data));
+        // printf("Response sent to browser!\n");
+        // close(client_fd);
     }
-    
+
     close(server_fd);
     return 0;
 }
